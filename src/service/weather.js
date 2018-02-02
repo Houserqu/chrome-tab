8793617b86a99a7832e3aeb210859eca7d2ef008
houser <houserqu@qq.com>
2018-02-02 13:48:21 +0800

    weather

--- src/service/weather.js
+++ src/service/weather.js
@@ -23,3 +23,25 @@ export function getWeather(params) {
   })
 }
 
+export function getCity(params) {
+  let url = urlParams("http://jisutqybmf.market.alicloudapi.com/weather/query?", params);
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+      method: 'get',
+      headers : {
+        "Authorization": "APPCODE 557d3e7aaf744971bca3ed75c7c0421f"
+      },
+    }).then((response) => {
+      if(response.status == 200){
+        return response.json()
+      } else {
+        reject(response.statusText);
+      }
+    }).then((responseData) => {
+      resolve(responseData)
+    }).catch((err) => {
+      reject(err);
+    });
+  })
+}
+
